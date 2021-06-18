55cb2616215eccb4fe4ab1af6cb58dc1f47b5124
Jonathan Vexler <jon@JVs-MacBook-Pro.lan>
2021-06-18 18:28:56 -0500

    test volume display

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
 import json
+import time
 stream = os.popen("minidsp -o json")
+volume = ""
+oldvolume=""
+while (True):
+    mdspdata = json.loads(stream.read())
+    oldvolume = volume
+    volume = mdspdata["master"]["volume"]
+    if oldvolume != volume:
+        print(volume)
+    stream.write("minidsp -o json")
+    time.sleep(0.25)
+    
+
 #stream.write("minidsp -o json")
-mdspdata = json.loads(stream.read())
-for key in mdspdata:
-    print("*******************")
-    print(key)
-    print(mdspdata[key])
+
+# for key in mdspdata:
+#     print("*******************")
+#     print(key)
+#     print(mdspdata[key])
