0b84ba6740f9aab1bb36a6d40e644c8724a3faeb
Jonathan Vexler <jon@JVs-MacBook-Pro.lan>
2021-06-18 18:40:08 -0500

    test source

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ volume = ""
 oldvolume=""
 mute=False
 oldmute=False
+source=""
+oldsource=""
 while (True):
     oldvolume = volume
     oldmute = mute
+    oldsource = source
 
     mdspdata = json.loads(stream.read())
     volume = mdspdata["master"]["volume"]
     mute = mdspdata["master"]["mute"]
+    source = mdspdata["master"]["source"]
+    if source != oldsource:
+        print("*****Source: " + source + "*****")
     if mute != oldmute:
         if mute:
             print("*****System Mute On*****")
         else:
             print("*****System Mute Off*****")
     if oldvolume != volume:
-        if mute:
-            print("m " + str(volume))
-        else:
-            print(volume)
+        print(volume)
     stream = os.popen("minidsp -o json")
     time.sleep(0.25)
     
