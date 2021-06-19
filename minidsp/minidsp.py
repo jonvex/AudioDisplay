20625edda9b31c70c207aec2e7f1c828189aeae4
Jonathan Vexler <jon@JVs-MacBook-Pro.lan>
2021-06-18 19:04:45 -0500

    minidsp class test3

--- minidsp/minidsp.py
+++ minidsp/minidsp.py
@@ -7,34 +7,34 @@ UPDATE_RATE = 0.25
 class Minidsp:
 
     def __init__(self):
-        self.time = 0
-        self.mute = ""
-        self.volume = ""
-        self.source = ""
+        self.__time = 0
+        self.__mute = ""
+        self.__volume = ""
+        self.__source = ""
         self.__update()
 
     def __update(self):
         newtime = time.time_ns()
-        if (newtime - self.time) > (1e9 * UPDATE_RATE):
+        if (newtime - self.__time) > (1e9 * UPDATE_RATE):
             print("acutally updated")
             stream = os.popen("minidsp -o json")
             mdspdata = json.loads(stream.read())
-            self.volume = mdspdata["master"]["volume"]
-            self.mute = mdspdata["master"]["mute"]
-            self.source = mdspdata["master"]["source"]
-            self.time = newtime
+            self.__volume = mdspdata["master"]["volume"]
+            self.__mute = mdspdata["master"]["mute"]
+            self.__source = mdspdata["master"]["source"]
+            self.__time = newtime
 
     def volume(self):
         self.__update()
-        return self.volume
+        return self.__volume
 
     def mute(self):
         self.__update()
-        return self.mute
+        return self.__mute
 
     def source(self):
         self.__update()
-        return self.source
+        return self.__source
     
     
 
