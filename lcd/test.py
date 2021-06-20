87d937ee40ba528c8c1234239e5aa243931fe790
Jonathan Vexler <jon@JVs-MacBook-Pro.lan>
2021-06-20 16:51:06 -0400

    test lcd

--- lcd/test.py
+++ lcd/test.py
@@ -1,38 +1,29 @@
 import I2C_LCD_driver
 import time
+import atexit
+
+HOLD_TIME = 0.25
+CLEAR_TIME = 0.1
 
 mylcd = I2C_LCD_driver.lcd()
 
-# keepgoing = True
-# line = ""
-# lines = {}
-# while (keepgoing):
-#     line = input("Enter Line Number: ")
-#     if line == 0:
-#         keepgoing = False
-#     else:
-#         lines[line] = input("Enter Text For Line " + str(line) + ":")
+def exit_handler():
+    time.sleep(2)
+    mylcd.lcd_clear()
+    mylcd.backlight(0)
 
-# keepgoing = (len(lines) != 0)
-# str_pad = " " * 20
-# for line in lines:
-#     lines[line] = str_pad + lines[line]
+atexit.Register(exit_handler)
 
-# while keepgoing:
-#     line = input("Enter Line Number: ")
-#     for i in range (0)
-#     for line in lines:
 mylcd.backlight(1)
 numstr = "Number:"
 numstr = numstr.rjust(17)
 emptyln = " " * 20
-for i in range(100):
-    mylcd.lcd_display_string(numstr + str(i).rjust(3),1)
-    time.sleep(0.25)
-    mylcd.lcd_display_string(numstr,1)
-    time.sleep(0.05)
+while True
+    for i in range(100):
+        mylcd.lcd_display_string(numstr + str(i).rjust(3),1)
+        time.sleep(HOLD_TIME)
+        mylcd.lcd_display_string(numstr,1)
+        time.sleep(CLEAR_TIME)
 
-time.sleep(2)
-mylcd.lcd_clear()
-mylcd.backlight(0)
+
 
