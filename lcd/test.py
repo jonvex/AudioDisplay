cabf248bb14b6f49d486fddfc90174f0e2d9f877
Jonathan Vexler <jon@JVs-MacBook-Pro.lan>
2021-06-20 16:42:50 -0400

    test lcd

--- lcd/test.py
+++ lcd/test.py
@@ -24,16 +24,14 @@ mylcd = I2C_LCD_driver.lcd()
 #     for line in lines:
 mylcd.backlight(1)
 numstr = "Number:"
-numstr = numstr.rjust(16)
+numstr = numstr.rjust(17)
 emptyln = " " * 20
 for i in range(100):
-    mylcd.lcd_display_string(numstr + str(i).rjust(4),1)
+    mylcd.lcd_display_string(numstr + str(i).rjust(3),1)
     time.sleep(0.25)
     mylcd.lcd_display_string(numstr,1)
+    time.sleep(0.05)
 
-time.sleep(2)
-mylcd.lcd_clear()
-mylcd.lcd_display_string("Goodbye!!!",2,6)
 time.sleep(2)
 mylcd.lcd_clear()
 mylcd.backlight(0)
